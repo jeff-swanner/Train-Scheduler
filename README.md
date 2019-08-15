f5bf6d61a210d0a68afa2d6f278ec7a4d6b78084
Jeff Swanner <jeffswanner93@gmail.com>
2019-08-14 23:15:24 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Train-Scheduler
+
+This project train scheduling demo that stores train information and calculates the next train arrival. 
+
+Firebase is used to store the train data. Javascript and moment.js are utilized for the logic to populate the table values and calculate train arrival information. Bootstrap and some custom css are used for the front end formatting. 
+
+The source code repository can be found at: https://github.com/jeff-swanner/Train-Scheduler and the Github pages deploy can be found at: https://jeff-swanner.github.io/Train-Scheduler/
+
+## Getting Started
+
+To get started clone the code to your local machine and open the index.html file. 
+
+## Authors
+
+Jeff Swanner
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // Setup for Firebase Config
     var config = {
         apiKey: "AIzaSyAOHxmgnq2mE_OV98WbqXq-V2sI_j4SU8U",
         authDomain: "train-scheduler-ac26f.firebaseapp.com",
@@ -9,70 +10,11 @@ $(document).ready(function(){
         appId: "1:583142225513:web:b042035044abc900"
     };
 
+    // Initializes firebase and database
     firebase.initializeApp(config);
     var database = firebase.database();
 
-    function minutesAwayCalc(firstTrainTime,trainFrequency){
-        var currentTime = moment().format("HH:mm");
-        trainFrequency = parseInt(trainFrequency);
-        var timeDifference = moment(currentTime,"HH:mm").diff(moment(firstTrainTime,"HH:mm"),"minutes");
-        if (timeDifference >= 0) {
-            var minutesAway = trainFrequency-timeDifference%trainFrequency;
-        } else {
-            var minutesAway = -1*timeDifference;
-        }
-        return minutesAway;
-    };
-
-    function nextArrivalCalc(minutesAway){
-        var currentTime = moment().format("HH:mm");
-        var nextArrival = moment(currentTime, "HH:mm").add(minutesAway, "minutes").format("hh:mm A");
-        return nextArrival;
-    };
-
-    $(document).on('click', '#submit', function(event){
-        event.preventDefault();
-
-        var trainName = $("#trainNameInput").val();
-        var destination = $("#destinationInput").val();
-        var firstTrainTime = $("#firstTrainTimeInput").val();
-        var trainFrequency = $("#trainFrequencyInput").val();
-
-        var d = moment(firstTrainTime,'HH:mm');
-        if(d == null || !d.isValid()) {
-            var trainTimeCheck = false;
-        } else {
-            var trainTimeCheck = true;
-        };
-
-        if (Number.isInteger(parseFloat(trainFrequency)) && parseFloat(trainFrequency)>0) {
-            var trainFrequencyCheck = true;
-        } else {
-            var trainFrequencyCheck = false;
-        };
-
-        if (trainTimeCheck === false) {
-            alert("Check First Train Time input for proper formatting");
-            
-        } else if (trainFrequencyCheck === false) {
-            alert("Train Frequency Input is not a valid positive integer");
-        } else {
-            database.ref("/trains/").push({
-                trainName: trainName,
-                destination: destination,
-                firstTrainTime: firstTrainTime,
-                trainFrequency: trainFrequency
-            });
-            $("#trainNameInput").val("");
-            $("#destinationInput").val("");
-            $("#firstTrainTimeInput").val("");
-            $("#trainFrequencyInput").val("");
-        };
-    });
-
-    var refresh = setInterval(trainTable, 5*1000);
-    trainTable();
-
+    // Grabs data from firebase and updates the train arrival table
     function trainTable() {
         $("#tableBody").empty();
         database.ref("/trains/").on("child_added", function(snapshot) {
@@ -107,7 +49,79 @@ $(document).ready(function(){
             $("#tableBody").append(tableRow);
         });
     };
+
+    // Updates table if train is removed, removal button not yet implemented
     database.ref("/trains/").on("child_removed", function() {
         trainTable();
     });
+
+    // Calculates train minutes away with moment based on first train time and train frequency
+    function minutesAwayCalc(firstTrainTime,trainFrequency){
+        var currentTime = moment().format("HH:mm");
+        trainFrequency = parseInt(trainFrequency);
+        var timeDifference = moment(currentTime,"HH:mm").diff(moment(firstTrainTime,"HH:mm"),"minutes");
+        if (timeDifference >= 0) {
+            var minutesAway = trainFrequency-timeDifference%trainFrequency;
+        } else {
+            var minutesAway = -1*timeDifference;
+        }
+        return minutesAway;
+    };
+
+    // Calculates next train arrival based on minutes away
+    function nextArrivalCalc(minutesAway){
+        var currentTime = moment().format("HH:mm");
+        var nextArrival = moment(currentTime, "HH:mm").add(minutesAway, "minutes").format("hh:mm A");
+        return nextArrival;
+    };
+
+    // Takes input for new train entry and pushes to firebase
+    $(document).on('click', '#submit', function(event){
+        event.preventDefault();
+
+        var trainName = $("#trainNameInput").val();
+        var destination = $("#destinationInput").val();
+        var firstTrainTime = $("#firstTrainTimeInput").val();
+        var trainFrequency = $("#trainFrequencyInput").val();
+
+        // Checks that train time entered is a valid military time
+        var d = moment(firstTrainTime,'HH:mm');
+        if(d == null || !d.isValid()) {
+            var trainTimeCheck = false;
+        } else {
+            var trainTimeCheck = true;
+        };
+
+        // Checks that train frequency input is a valid positive integer
+        if (Number.isInteger(parseFloat(trainFrequency)) && parseFloat(trainFrequency)>0) {
+            var trainFrequencyCheck = true;
+        } else {
+            var trainFrequencyCheck = false;
+        };
+
+        // Alerts on invalid data entry and doesn't push to firebase
+        if (trainTimeCheck === false) {
+            alert("Check First Train Time input for proper formatting");
+        } else if (trainFrequencyCheck === false) {
+            alert("Train Frequency Input is not a valid positive integer");
+        } else {
+            database.ref("/trains/").push({
+                trainName: trainName,
+                destination: destination,
+                firstTrainTime: firstTrainTime,
+                trainFrequency: trainFrequency
+            });
+            $("#trainNameInput").val("");
+            $("#destinationInput").val("");
+            $("#firstTrainTimeInput").val("");
+            $("#trainFrequencyInput").val("");
+        };
+    });
+
+    // Refreshes screen every 5 seconds
+    // Originally set to a minute but sometimes the table glitches when inputing new train info and lists the train several times
+    var refresh = setInterval(trainTable, 5*1000);
+
+    // Calls the initial train table function on page load
+    trainTable();
 });
